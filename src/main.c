002f6c7b83e6edb26fcfc859a157f1ce186ba3b7
jack-stow <getjackstow@gmail.com>
2026-06-12 13:26:53 -0400

    moving main helper function declarations to header file

--- src/main.c
+++ src/main.c
@@ -4,37 +4,14 @@
 #include "init.h"
 #include "input.h"
 #include "main.h"
-#include "boid.h"
 #include "flockbehavior.h"
-#include "poi.h"
-#include "obstacles.h"
 #include "camera.h"
-#include "workerpool.h"
 #include "stats.h"
-#include "uniformgrid.h"
 
 
 App    app;
 Stats stats;
 Camera camera;
-
-
-void printStats(void);
-vec2 GetCameraInputDirection();
-void DrawBorders();
-void DrawBoidsBatchedPoints(Camera* camera, Boid* boids, int numBoids, SimulationParameters* sim, bool rebuildPoints);
-void HandleObstacleInput(Obstacles** obstacles);
-void RunFlockWorkers(WorkerPool* workerpool, UniformGrid* grid, int boidCount, Boid** boids, Boid** boidsNext, Obstacles* obstacles, SimulationParameters* sim, PointOfInterest* pointsOfInterest, int poiCount, real deltaTime);
-
-void DrawBoidsBatchedPointsInterpolated(
-	Camera* camera,
-	const Boid* previous,
-	const Boid* current,
-	int numBoids,
-	SimulationParameters* sim,
-	real alpha,
-	bool rebuildPoints);
-
 
 int main(int argc, char* argv[])
 {
--- src/main.h
+++ src/main.h
@@ -1,2 +1,30 @@
 
+#include "boid.h"
+#include "obstacles.h"
+#include "workerpool.h"
+#include "uniformgrid.h"
+#include "poi.h"
+
+void printStats(void);
+
+vec2 GetCameraInputDirection();
+void DrawBorders();
+
+void DrawBoidsBatchedPoints(Camera* camera, Boid* boids, int numBoids, SimulationParameters* sim, bool rebuildPoints);
+
+void HandleObstacleInput(Obstacles** obstacles);
+
+void RunFlockWorkers(WorkerPool* workerpool, UniformGrid* grid, int boidCount, Boid** boids, Boid** boidsNext, Obstacles* obstacles, SimulationParameters* sim, PointOfInterest* pointsOfInterest, int poiCount, real deltaTime);
+
+
+void DrawBoidsBatchedPointsInterpolated(
+	Camera* camera,
+	const Boid* previous,
+	const Boid* current,
+	int numBoids,
+	SimulationParameters* sim,
+	real alpha,
+	bool rebuildPoints);
+
+
 int main(int argc, char *argv[]);
